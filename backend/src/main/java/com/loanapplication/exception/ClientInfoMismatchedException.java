a9351d9f3465d6efa1e6c7edf871ded29282bac1
uralkeser <osmanuralkeser@gmail.com>
2022-01-30 00:38:15 +0300

    code refactoring for testing

--- backend/src/main/java/com/loanapplication/exception/ClientInfoMismatchedException.java
+++
@@ -1,7 +0,0 @@
-package com.loanapplication.exception;
-
-public class ClientInfoMismatchedException extends RuntimeException{
-    public ClientInfoMismatchedException(String message){
-        super(message);
-    }
-}
